fd2f3d639023566597e67bc9af69871ece72aa97
Fran@Fran <>
2016-02-11 13:14:38 -0300

    Consultas DB - Funcionalidad Calculo  / Validaciones

--- Proyecto TEST/SEDIRA-TEST/src/sedira/DatosValidacionesCalculo.java
+++ Proyecto TEST/SEDIRA-TEST/src/sedira/DatosValidacionesCalculo.java
@@ -9,48 +9,115 @@ import sedira.model.Calculo;
 import sedira.model.Paciente;
 import sedira.model.Phantom;
 import sedira.model.Organo;
+import sedira.model.Radionuclido;
 
 /**
+ * Clase que define y guarda un calculo en tiempo de ejecucion
  *
- * @author Fran
+ * @author Hefner Francisco, Quelin Pablo
  */
 public class DatosValidacionesCalculo {
-    
-    
-        private Calculo CalculoActual;
-        private Paciente PacienteActual;
-        private Phantom PhantomActual;
-        private Organo OrganoActual;
-       
-        
-        
-        private void LimpiarVariables()
-        {
-            
+
+    private Calculo CalculoActual;
+    private Paciente PacienteActual;
+    private Phantom PhantomActual;
+    private Organo OrganoActual;
+    private Radionuclido RadionuclidoActual;
+
+    private void limpiarVariables() {
+        this.CalculoActual = null;
+        this.PacienteActual = null;
+        this.PhantomActual = null;
+        this.OrganoActual = null;
+        this.RadionuclidoActual = null;
+    }
+
+    /* Validaciones por cada objeto antes de finalizar el proceso y llenar la entidad cálculo */
+    /* 1 - Que este en la base de datos el objeto (Paciente, Phantom, Organo, Radionuclido)
+     2 - Que el paciente este en tratamiento
+     3 - add..
+     */
+    private boolean validarPaciente() {
+
+        /*Ej: Aplicar hash MD5 al objeto y compararlo con ConsultasDB.Obtener(Paciente)*/
+        this.PhantomActual = null;
+        this.OrganoActual = null;
+        this.RadionuclidoActual = null;
+        this.CalculoActual = null;
+
+        return true;
+    }
+
+    private boolean validarPhantom() {
+
+        this.OrganoActual = null;
+        this.RadionuclidoActual = null;
+        this.CalculoActual = null;
+
+        return true;
+    }
+
+    private boolean validarOrgano() {
+
+        this.RadionuclidoActual = null;
+        this.CalculoActual = null;
+
+        return true;
+    }
+
+    private boolean validaradionuclidoActual() {
+
+        this.CalculoActual = null;
+
+        return true;
+    }
+
+    public boolean setPaciente(Paciente miPaciente) {
+        this.PacienteActual = miPaciente;
+        return validarPaciente();
+    }
+
+    public boolean setPhantom(Phantom miPhantom) {
+        this.PhantomActual = miPhantom;
+
+        return validarPhantom();
+    }
+
+    public boolean setOrgano(Organo miOrgano) {
+        this.OrganoActual = miOrgano;
+
+        return validarOrgano();
+    }
+
+    public boolean setRadionuclido(Radionuclido miRadionuclido) {
+        this.RadionuclidoActual = miRadionuclido;
+
+        return validaradionuclidoActual();
+    }
+
+    public void IniciarCalculo() {
+        limpiarVariables();
+    }
+
+    public String EstadoActual() {
+        if (this.PacienteActual == null) {
+            return "Paciente";
+        } else {
+            if (this.PhantomActual == null) {
+                return "Phantom";
+            } else {
+                if (this.OrganoActual == null) {
+                    return "Organo";
+                } else {
+                    if (this.RadionuclidoActual == null) {
+                        return "RadioNuclido";
+                    } else {
+                        return "Completo";
+                    }
+                }
+            }
         }
-        
-        public boolean setPaciente(Paciente MiPaciente)
-        {
-            return true;
-        }
-        
-      public boolean setPhantom(Phantom MiPhantom)
-      {
-          
-          return true;
-      }       
-      public boolean setOrgano(Organo MiOrgano)
-      {
-          
-          return true;
-      }  
-        
-       public void IniciarCalculo()
-       {
-           LimpiarVariables();
-           
-           
-       }
-                        
-        
+
+    }
+
 }
--- Proyecto TEST/SEDIRA-TEST/src/sedira/vistas/CalculoController.java
+++ Proyecto TEST/SEDIRA-TEST/src/sedira/vistas/CalculoController.java
@@ -16,23 +16,26 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import sedira.DatosValidacionesCalculo;
 
 /**
- * FXML Controller class
- * Clase controladora para el proceso de Calculo. 
- * @author Hefner Francisco, Quelin Pablo 
+ * FXML Controller class Clase controladora para el proceso de Calculo.
+ *
+ * @author Hefner Francisco, Quelin Pablo
  */
 public class CalculoController implements Initializable {
+
     @FXML
     private TabPane tabPaneCalculo;
-    @FXML
-    private Tab tabPaciente;
+
     @FXML
     private TableView<?> griListaPacientes;
     @FXML
@@ -44,6 +47,8 @@ public class CalculoController implements Initializable {
     @FXML
     private TableColumn<?, ?> clApellido;
     @FXML
+    private Tab tabPaciente;
+    @FXML
     private Tab tabPhantom;
     @FXML
     private Tab tabOrganoTejido;
@@ -51,76 +56,122 @@ public class CalculoController implements Initializable {
     private Tab tabRadionuclido;
     @FXML
     private Tab tabCalcular;
-    @FXML 
+    @FXML
     private Pane pnlPhantom;
-    @FXML 
+    @FXML
     private Pane pnlPaciente;
-    @FXML 
+    @FXML
     private Pane pnlRadionuclido;
-    @FXML 
+    @FXML
     private Pane pnlOrgano;
+    @FXML
+    private Button btnSiguiente;
+    @FXML
+    private Button btnAtras;
+    @FXML
+    private Button btnCancelar;
+
     private PestañaOrganoController organoController;
-    
-   
+
     /**
      * Initializes the controller class.
      */
+    DatosValidacionesCalculo NuevoCalculo = new DatosValidacionesCalculo();
+
+    SingleSelectionModel<Tab> listaTABS = tabPaneCalculo.getSelectionModel();
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-                        
-        try { 
-             Node NodoPhantom;
-            NodoPhantom = (Node)FXMLLoader.load(getClass().getResource("PestañaPhantom.fxml"));
-             pnlPhantom.getChildren().setAll(NodoPhantom);
-             
-             Node NodoPaciente;
-            NodoPaciente = (Node)FXMLLoader.load(getClass().getResource("PestañaPaciente.fxml"));
-             pnlPaciente.getChildren().setAll(NodoPaciente);
-             
-             Node NodoRadionuclido;
-            NodoRadionuclido = (Node)FXMLLoader.load(getClass().getResource("PestañaRadionuclido.fxml"));
-             pnlRadionuclido.getChildren().setAll(NodoRadionuclido);
 
-             Node NodoOrgano;
-            NodoOrgano = (Node)FXMLLoader.load(getClass().getResource("PestañaOrgano.fxml"));
-             pnlOrgano.getChildren().setAll(NodoOrgano);
-             
-             
+        try {
+            /* SE INICIA EL CÁLCULO, Y SE CARGAN LAS PESTAÑAS */
+            NuevoCalculo.IniciarCalculo();
+
+            Node NodoPhantom;
+            NodoPhantom = (Node) FXMLLoader.load(getClass().getResource("PestañaPhantom.fxml"));
+            pnlPhantom.getChildren().setAll(NodoPhantom);
+
+            Node NodoPaciente;
+            NodoPaciente = (Node) FXMLLoader.load(getClass().getResource("PestañaPaciente.fxml"));
+            pnlPaciente.getChildren().setAll(NodoPaciente);
+
+            Node NodoRadionuclido;
+            NodoRadionuclido = (Node) FXMLLoader.load(getClass().getResource("PestañaRadionuclido.fxml"));
+            pnlRadionuclido.getChildren().setAll(NodoRadionuclido);
+
+            Node NodoOrgano;
+            NodoOrgano = (Node) FXMLLoader.load(getClass().getResource("PestañaOrgano.fxml"));
+            pnlOrgano.getChildren().setAll(NodoOrgano);
+
         } catch (IOException ex) {
             Logger.getLogger(CalculoController.class.getName()).log(Level.SEVERE, null, ex);
         }
-         
-    }   
-    
-    /**
-     * Posiblemente dentro de este controlador estara el control de los objetos necesarios de pestaña para otra pestaña. 
-     */
-    public void setChoiceOrgano(){
-        //organoController.initListaOrgano();
-        
     }
-    
-   
-    
-     @FXML
-    private void btnSiguiente_click() throws IOException
-    {        
-       
-      System.out.print(tabPhantom.getId());
-       System.out.print(tabOrganoTejido.getId());
-      
-      // tabPhantom.getId();
-       
+
+    public void SeleccionPestaña() {
+        String EstadoActual = NuevoCalculo.EstadoActual();
+
+        if (EstadoActual.equals("Paciente")) {
+            tabPaciente.setDisable(false);
+            listaTABS.select(tabPaciente);
+
+            tabPhantom.setDisable(true);
+            tabOrganoTejido.setDisable(true);
+            tabRadionuclido.setDisable(true);
+            tabCalcular.setDisable(true);
+        }
+        if (EstadoActual.equals("Phantom")) {
+            tabPhantom.setDisable(false);
+            listaTABS.select(tabPhantom);
+
+            tabPaciente.setDisable(true);
+            tabOrganoTejido.setDisable(true);
+            tabRadionuclido.setDisable(true);
+            tabCalcular.setDisable(true);
+        }
+        if (EstadoActual.equals("Organo")) {
+            tabOrganoTejido.setDisable(false);
+            listaTABS.select(tabOrganoTejido);
+
+            tabPaciente.setDisable(true);
+            tabPhantom.setDisable(true);
+            tabRadionuclido.setDisable(true);
+            tabCalcular.setDisable(true);
+        }
+        if (EstadoActual.equals("RadioNuclido")) {
+            tabRadionuclido.setDisable(false);
+            listaTABS.select(tabRadionuclido);
+
+            tabPaciente.setDisable(true);
+            tabPhantom.setDisable(true);
+            tabOrganoTejido.setDisable(true);
+            tabCalcular.setDisable(true);
+        }
+        if (EstadoActual.equals("Completo")) {
+            tabCalcular.setDisable(false);
+            listaTABS.select(tabCalcular);
+
+            tabRadionuclido.setDisable(true);
+            tabPaciente.setDisable(true);
+            tabPhantom.setDisable(true);
+            tabOrganoTejido.setDisable(true);
+        }
+
     }
-     @FXML
-    private void btnAtras_click() throws IOException
-    { 
-       
+
+    @FXML
+    private void btnSiguiente_click() throws IOException {
+        SeleccionPestaña();
     }
-     @FXML
-     private void btnCancelar_click() throws IOException
-    { 
-       
+
+    @FXML
+    private void btnAtras_click() throws IOException {
+        SeleccionPestaña();
     }
-    
+
+    @FXML
+    private void btnCancelar_click() throws IOException {
+
+    }
+
 }
