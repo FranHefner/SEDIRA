df3ae8eb7ad920b14553b938a1c2e17dd6eb07e1
Pablo Quelin <pablo.quelin@gmail.com>
2017-05-28 21:13:49 -0300

    Terminado el About

--- Proyecto TEST/SEDIRA-TEST_db/src/sedira/vistas/AyudaController.java
+++ Proyecto TEST/SEDIRA-TEST_db/src/sedira/vistas/AyudaController.java
@@ -5,7 +5,10 @@
  */
 package sedira.vistas;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
+import java.net.URLConnection;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -18,6 +21,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import sedira.model.ConexionDB;
@@ -36,7 +40,9 @@ public class AyudaController implements Initializable {
     @FXML
     private Label lblVersion;
     @FXML
-    private TextField txtDbInfo;
+    private Label lblActualizacion;
+    @FXML
+    private TextArea txtAreaInfo;
     @FXML
     private TextField txtSystemInfo;
    
@@ -49,33 +55,57 @@ public class AyudaController implements Initializable {
 
         lblInfoTitulo.setText("Sistema para estimar la dosis interna de radiación absorbida en tratamientos de medicina nuclear");
         lblVersion.setText("Versión 5.0");
+        lblActualizacion.setText("Última actualización: 28/05/2017");
+
+      
+
         try {
-            obtenerInfoDb();
+            obtenerInfo();
         } catch (SQLException ex) {
             Logger.getLogger(AyudaController.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
+   
+
     @FXML
     private void btnCerrar_click(ActionEvent event) {
         Stage stage = (Stage) btnCerrar.getScene().getWindow();
-
         stage.close();
     }
 
-    private void obtenerInfoDb() throws SQLException {
+    private void obtenerInfo() throws SQLException {
         //Instancia de conexion
         ConexionDB conexion = new ConexionDB();
         DatabaseMetaData meta = conexion.getConnection().getMetaData();
-        String Version = meta.getDatabaseProductVersion();
-        //txtDbInfo.setText(Version);
+        String nameOS = "os.name";
+        String versionOS = "os.version";
+        String architectureOS = "os.arch";
+
+        String info = "Versión servidor base de datos: \n" + meta.getDatabaseProductVersion();
+
+        info += "\nProcesadores Disponibles (cores): "
+                + Runtime.getRuntime().availableProcessors();
+        /* Total amount of free memory available to the JVM */
+        info += "\nMemoria libre (bytes): "
+                + Runtime.getRuntime().freeMemory();
+        /* This will return Long.MAX_VALUE if there is no preset limit */
+        long maxMemory = Runtime.getRuntime().maxMemory();
+        /* Maximum amount of memory the JVM will attempt to use */
+        info += "\nMemoria máxima (bytes): "
+                + (maxMemory == Long.MAX_VALUE ? "no limit" : maxMemory);
+        /* Total memory currently available to the JVM */
+        info += "\nMemoria total disponible para JVM (bytes): "
+                + Runtime.getRuntime().totalMemory();
+        info += "\n\nInformación sobre el Sistema Operativo: ";
+        info += "\nNombre: "
+                + System.getProperty(nameOS);
+        info += "\nVersion:  "
+                + System.getProperty(versionOS);
+        info += "\nArquitectura: "
+                + System.getProperty(architectureOS);
+        txtAreaInfo.setText(info);
 
     }
-     private void obtenerSystemInfo() throws SQLException {
-        
-
-    }
-    
-    
 
 }
